27590508a9334acceed8bb2e41ac8c53ca6ded23
ayaaribi123 <ayaaribi11@gmail.com>
2023-12-08 11:28:52 +0100

    dart function

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,6 +1,8 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int team1 = (teamA['Free throws'] ?? 0) + (teamA['2 pointers'] ?? 0) * 2 + (teamA['3 pointers'] ?? 0) * 3;
-  int team2 = (teamB['Free throws'] ?? 0) + (teamB['2 pointers'] ?? 0) * 2 + (teamB['3 pointers'] ?? 0) * 3;
+  int team1 = (teamA['Free throws'] ?? 0) + 
+  (teamA['2 pointers'] ?? 0) * 2 + (teamA['3 pointers'] ?? 0) * 3;
+  int team2 = (teamB['Free throws'] ?? 0) + 
+  (teamB['2 pointers'] ?? 0) * 2 + (teamB['3 pointers'] ?? 0) * 3;
 
   if (team1 > team2) {
     return 1;
