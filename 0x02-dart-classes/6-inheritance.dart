830a8e202e2f3750506d78ec9e4b121e3e5e2b04
ayaaribi123 <ayaaribi11@gmail.com>
2023-12-08 15:18:47 +0100

    dart classes

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,20 +1,20 @@
 import '6-password.dart';
 import 'dart:convert';
 
-class User {
+class User extends Password{
   int id;
   String name;
   int age;
   double height;
-  String? user_password ='';
+  String user_password;
 
   User({
     required this.id,
     required this.name,
     required this.age,
     required this.height,
-    this.user_password,
-  });
+     required this.user_password,
+  }) : super(password: user_password);
 
   Map<String, dynamic> toJson() {
     return {
@@ -22,23 +22,20 @@ class User {
       'name': name,
       'age': age,
       'height': height,
-      //'user_password': user_password?.toString(),
     };
   }
 
-  static User fromJson(Map<dynamic, dynamic> userJson) {
+  static User fromJson(Map<dynamic, dynamic> userJson){
     return User(
       id: userJson['id'],
       name: userJson['name'],
       age: userJson['age'],
       height: userJson['height'],
-      user_password: '', 
+      user_password: userJson["user_password"],
     );
-  }
+  } 
 
-  @override
-  String toString() {
-     final pass = Password(password: this.user_password!);
-    return 'User(id: $id, name: $name, age: $age, height: $height, password:  ${pass.isValid()}))';
+ String toString() {
+    return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})";
   }
 }
